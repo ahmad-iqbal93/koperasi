0ea28e3756d2fe4e34252320b9bebda4d97ac195
ahmad-iqbal93 <iqbal.seiawan93@gmail.com>
2020-08-16 04:22:17 +0700

    update backend & form register dengan json

--- src/components/pages/register/Register.jsx
+++ src/components/pages/register/Register.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Register.css'
+import { useState } from 'react'
 
 const Register = () => {
+
+    let [userInput, setUserInput] = useState({
+        userName: '',
+        email: '',
+        password: '',
+        password2: '',
+    })
+
+    let handleSubmit = (e) => {
+        e.preventDefault()
+
+        fetch('http://localhost:3005/register', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                userName: userInput.userName,
+                email: userInput.email,
+                password: userInput.password,
+                password2: userInput.password2
+            })
+        })
+
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
+
+
     return (
 
-        <div className="container-fluid bg-gradient-dark h-100" style={{
-            height: '100vh !important',
+        <div className="container-fluid bg-gradient-dark" style={{
             width: '100vw !important',
             display: 'flex',
             padding: '33.5px 0',
@@ -23,31 +55,52 @@ const Register = () => {
                                 <div className="text-center">
                                     <h1 className="h4 text-gray-900 mb-4">Buat Akun Baru!</h1>
                                 </div>
-                                <form className="user">
+                                <form className="user" onSubmit={handleSubmit}>
                                     <div className="form-group row">
-                                        <div className="col-sm-6 mb-3 mb-sm-0">
-                                            <input type="text" className="form-control form-control-user" id="exampleFirstName" placeholder="First Name" />
-                                        </div>
-                                        <div className="col-sm-6">
-                                            <input type="text" className="form-control form-control-user" id="exampleLastName" placeholder="Last Name" />
+                                        <div className="col-sm-12 mb-3 mb-sm-0">
+                                            <input type="text" className="form-control form-control-user" id="exampleFirstName" placeholder="User Name" onChange={(e) => {
+                                                setUserInput({
+                                                    ...userInput,
+                                                    userName: e.target.value
+                                                })
+                                                console.log(userInput);
+                                            }} />
                                         </div>
                                     </div>
                                     <div className="form-group">
-                                        <input type="email" className="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address" />
+                                        <input type="email" className="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address" onChange={(e) => {
+                                            setUserInput({
+                                                ...userInput,
+                                                email: e.target.value
+                                            })
+                                            console.log(userInput);
+                                        }} />
                                     </div>
                                     <div className="form-group row">
                                         <div className="col-sm-6 mb-3 mb-sm-0">
-                                            <input type="password" className="form-control form-control-user" id="exampleInputPassword" placeholder="Password" />
+                                            <input type="password" className="form-control form-control-user" id="exampleInputPassword" placeholder="Password" onChange={(e) => {
+                                                setUserInput({
+                                                    ...userInput,
+                                                    password: e.target.value
+                                                })
+                                                console.log(userInput);
+
+                                            }} />
                                         </div>
                                         <div className="col-sm-6">
-                                            <input type="password" className="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password" />
+                                            <input type="password" className="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password" onChange={(e) => {
+                                                setUserInput({
+                                                    ...userInput,
+                                                    password2: e.target.value
+                                                })
+                                                console.log(userInput);
+
+                                            }} />
                                         </div>
                                     </div>
-                                    <a href="login.html" className="btn btn-primary btn-user btn-block">
+                                    <button to='#' type='submit' className="btn btn-primary btn-user btn-block">
                                         Register Account
-                    </a>
-
-
+                                    </button>
                                 </form>
                                 <hr />
                                 <div className="text-center">
